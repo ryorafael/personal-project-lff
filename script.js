e2f9457faea65edf17d04502b4630f6ac8034cf6
Ryo Rafael <ryorafael18@gmail.com>
2024-01-23 12:07:32 +0100

    header added, reservation button function
    header added with html and css, functions added in JS for button on click

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //function ONclick button for reservations pop up calender, with hours and dates, and amount of people
-//objet id : name, time, date, how many people 
+//objet id : name, time, date, how many people
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".button-18");
+
+  function onButtonClick() {
+    alert("Button clicked!");
+  }
+
+  button.addEventListener("click", onButtonClick);
+});
